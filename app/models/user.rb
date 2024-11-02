dca475dbeadb87e9e1dedb0f95eaa2fa1c86d3d7
Luke Johnson <lukejohnson1@outlook.com>
2024-11-02 15:55:14 +1100

    email database validation & login button clean up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validate :email_uniquness_check, on: :create
+
   # Association for bookings
   has_many :bookings, dependent: :destroy
 
   # Association for trucks if the user is a companys
   has_many :trucks, dependent: :destroy
+
+  private
+
+  def email_uniquness_check
+    if User.exists?(email: email)
+      errors.add(:email,)
+    end
+  end
 end
