2c2146c7771d51225b824528c71b610c5c17dca4
Ashwin Almeida <almeidashwin@gmail.com>
2024-11-05 21:13:27 +1100

    search function with bookings

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -6,4 +6,12 @@ class Truck < ApplicationRecord
 
   # Allow photo attachment with Active Storage
   has_one_attached :photo
+
+    # Search though bookings
+    include PgSearch::Model
+    pg_search_scope :search_by_booking,
+    against: [ :title, :price, :description, :postcode],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
